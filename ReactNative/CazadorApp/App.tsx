e1edf4ece1234f645904df259bc92fee4f923923
Ana62-mp <ana.moposita84@gmail.com>
2026-09-21 20:40:17 -0500

    Refactor App component to initialize database and manage loading state

--- ReactNative/CazadorApp/App.tsx
+++ ReactNative/CazadorApp/App.tsx
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from "react";
+import { ActivityIndicator, View , Text} from "react-native";
+import { SQLiteProvider } from "expo-sqlite";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import { initDatabase } from "./src/database/db";
+import ListaScreen from "./src/screens/ListaScreen";
+import FormularioScreen from "./src/screens/FormularioScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [cargando, setCargando] = useState(true);
+
+  useEffect(() => {
+    async function iniciar() {
+      await initDatabase();
+      setCargando(false);
+    }
+
+    iniciar();
+  }, []);
+
+  if (cargando) {
+    return (
+      <View>
+        <ActivityIndicator size="large" />
+        <Text>Cargando base de datos...</Text>
+      </View>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SQLiteProvider databaseName="cazador.db">
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Lista"
+            component={ListaScreen}
+          />
+
+          <Stack.Screen
+            name="Formulario"
+            component={FormularioScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SQLiteProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
